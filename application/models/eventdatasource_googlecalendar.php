6b812d94888a4b260eb2127f1e4606384f18eeaa
GreenInfo Network <gin@greeninfo.org>
2014-07-01 16:57:33 -0700

    Request: Google Calendar event data sources, the driver now fetches both the Basic and the FUll and uses the Where: information from the Basic summary to get an address, then geocodes this using Bing and the existing API key (per the admiun UI Settings tab)  Not wholly okay with this, as the driver now requires intimate knowledge of the calling context (siteconfig) to determine whether there's a Bing API key...

--- application/models/eventdatasource_googlecalendar.php
+++ application/models/eventdatasource_googlecalendar.php
@@ -19,9 +19,10 @@ var $option_fields = array(
     'option9' => NULL,
 );
 
-// Google Calendar does have a Location field, but this isn't exposed via the API and is free-form text and not a location
-// locations such as "Meeting Room C" aren't useful to geocoding, leading to a very low success rate in real use cases
-var $supports_location = FALSE;
+// Google Calendar content has a Location field, but this isn't exposed via the API
+// so I was told to look for there "Where:" strings in the XML and just make do
+// real-world performance of this will be quite poor, and will make the datasource loads take a long time, likely even time out, ... but I was told to just do it and we'll figure that out later
+var $supports_location = TRUE;
 
 
 public function __construct() {
@@ -44,14 +45,23 @@ public function __construct() {
  *
  * This method throws an Exception in any case, either a EventDataSourceErrorException or EventDataSourceSuccessException
  * This allows for more complex communication than a simple true/false return, e.g. the name of the data source and an error code (number of events loaded)
+ *
+ * Big problem with Google's XML feeds:
+ *  the Basic feed has a summary with a Where: field and the location string which could hypothetically be geocoded,
+ *  while the Full feed is the one with the starttime and endtime and the descriptions
+ *  naturally we want both, so we hit the service twice then try to crosswalk the missing <summary> field onto the Full feed output
  */
 public function reloadContent() {
     // make sure no shenanigans: all Google feed URLs start with https and www.google.com
     // strip off any query params, and replace the default /basic "projection" component with a /full-noattendees so we get full info
+    // but also take one that does end in /basic/ so we can get at the precious <summary> tags
     $url = $this->url;
     if (strpos($url,'https://www.google.com/calendar/') !== 0) throw new EventDataSourceErrorException('Not a valid HTTPS URL at Google.');
     $url = preg_replace('/\?.*/', '', $url);
-    $url = preg_replace('/basic$/', 'full-noattendees', $url);
+    $url = preg_replace('/basic$/', '', $url);
+    $url = preg_replace('/full$/', '', $url);
+    $url = preg_replace('/full-noattendees$/', '', $url);
+    $url = preg_replace('/\/$/', '', $url);
 
     // expand upon the base URL, adding parameters to fetch only events today and foreard, to a maximum of 6 months
     // note that we "round" the current date to midnight, so we will show events today which have already started
@@ -67,35 +77,61 @@ public function reloadContent() {
         'max-results' => 250,
         'orderby' => 'starttime',
     );
-    $url = sprintf("%s?%s", $url, http_build_query($params) );
+    $full_url  = sprintf("%s/%s?%s", $url, 'full-noattendees', http_build_query($params) );
+    $basic_url = sprintf("%s/%s?%s", $url, 'basic',            http_build_query($params) );
 
     // fetch the XML, do the most basic check that a <?xml header is given
     // because some errors result in a HTML page or a brief text message
-    $xml = @file_get_contents($url);
-    if (substr($xml,0,6) != '<?xml ') throw new EventDataSourceErrorException('Non-XML response from the given URL. Not a calendar feed?');
+    $full_xml = @file_get_contents($full_url);
+    if (substr($full_xml,0,6) != '<?xml ') throw new EventDataSourceErrorException('Non-XML response from the given URL (full). Not a calendar feed?');
 
-    // replace $xml from the XML string to a XML parser, or die trying
+    $basic_xml = @file_get_contents($basic_url);
+    if (substr($basic_xml,0,6) != '<?xml ') throw new EventDataSourceErrorException('Non-XML response from the given URL (basic). Not a calendar feed?');
+
+    // replace the $xml variables with the parsed versions... or die trying
     try {
-        $xml = @new SimpleXMLElement($xml);
+        $full_xml = @new SimpleXMLElement($full_xml);
     } catch (Exception $e) {
-        throw new EventDataSourceErrorException('Could not parse response from the given URL. Not a calendar feed?');
+        throw new EventDataSourceErrorException('Could not parse response from the given URL (full). Not a calendar feed?');
+    }
+    try {
+        $basic_xml = @new SimpleXMLElement($basic_xml);
+    } catch (Exception $e) {
+        throw new EventDataSourceErrorException('Could not parse response from the given URL (basic). Not a calendar feed?');
     }
 
     // check for some known headers, and bail if we don't see them
-    $updated = (string) $xml->updated;
-    $title   = (string) $xml->title;
+    $updated = (string) $full_xml->updated;
+    $title   = (string) $full_xml->title;
     if (!$title or !$updated) throw new EventDataSourceErrorException('Got XML but no content.');
+
+    // the Basic feed, we really only want one thing: the <summary> tag for each event
+    // build a struct of these summaries keyed by the event's ID, and we can attach it to the real $entry items when we iterate over $full_xml in a little bit
+    $summaries = array();
+    foreach ($basic_xml->entry as $entry) {
+        $id  = basename( (string) $entry->id );
+        $sum = (string) $entry->summary;
+        $summaries[$id] = $sum;
+    }
 
     // finally got here, so we're good, dang that's a lot of validation; it'll pay off in the long run  ;)
     // take a moment and delete all of the old Events from this data source
     foreach ($this->event as $old) $old->delete();
 
-    // iterate over entries
-    $howmany = 0;
-    foreach ($xml->entry as $entry) {
+    // iterate over FULL entries and use them to construct our records
+    // splicing onto them the $summaries entry from the Basic feed   so we can extract the Where: info from them
+    // WARNING: as of now, the driver uses intimate knowledge of the framework context (siteconfig) which hypothetically should be in the Controller...
+    //          but how is that gonna happen, without the Controller introspecting the driver details in ways which also violate MVC...?
+    $bing_key   = $this->siteconfig->get('bing_api_key');
+    $howmany    = 0;
+    $no_geocode = 0;
+    foreach ($full_xml->entry as $entry) {
+        $id             = basename( (string) $entry->id );
+        $entry->summary = @$summaries[$id]; if (! $entry->summary) $entry->summary = "";
+
         $event = new Event();
         $event->eventdatasource_id  = $this->id;
-        $event->remoteid            = substr( (string) $entry->id, 0, 250);
+        $event->remoteid            = $id;
         $event->name                = substr( (string) $entry->title, 0, 100);
         $event->description         = (string) $entry->content;
 
@@ -134,14 +170,47 @@ public function reloadContent() {
         // ready!
         $event->save();
         $howmany++;
+
+        // afterthought
+        // the point of splicing on the <summary> tags was so we can parse them for probable location information via the Where: strings
+        // if the <summary> tag can't be found, or is empty, or just can't be geocoded, then skip it and increment that warning count
+        // also, bail condition: if we don't have a Bing API key configured, we must skip this
+        // WARNING: this does mean that the driver has intimate knowledge of the framework context (siteconfig) which violates MVC principles
+        //          but I'm not coming up with another way to do it, without the Controller getting into the Model and violating MVC anyway...
+        if (!$bing_key) { $no_geocode++; continue; }
+
+        $where = null;
+        preg_match('/[\r\n]+(<br>|<br \/>)Where: (.+?)[\r\n]+/', $entry->summary, $where);
+        $where = @$where[2];
+        if (! $where) { $no_geocode++; continue; }
+
+        $geocode = sprintf("http://dev.virtualearth.net/REST/v1/Locations?key=%s&output=json&query=%s",
+            $bing_key, urlencode($where)
+        );
+        $geocode = @json_decode(file_get_contents($geocode));
+        $lat = @$geocode->resourceSets[0]->resources[0]->geocodePoints[0]->coordinates[0];
+        $lng = @$geocode->resourceSets[0]->resources[0]->geocodePoints[0]->coordinates[1];
+        if (!$lat or !$lng) { $no_geocode++; continue; }
+
+        $loc = new EventLocation();
+        $loc->event_id      = $event->id;
+        $loc->latitude      = $lat;
+        $loc->longitude     = $lng;
+        $loc->title         = $event->name;
+        $loc->subtitle      = $where;
+        $loc->save();
     }
 
     // update our last_fetch date
     $this->last_fetch = time();
     $this->save();
 
-    // guess we're done and happyl; throw an error  (ha ha)
-    throw new EventDataSourceSuccessException("Successfully loaded $howmany events.");
+    // guess we're done and happy; throw an error  (ha ha)
+    $message = array();
+    $message[] = "Successfully loaded $howmany places.";
+    if ($no_geocode) $message[] = "$no_geocode places could not have their location geocoded.";
+    $message = implode("\n", $message);
+    throw new EventDataSourceSuccessException($message);
 }
 
 
