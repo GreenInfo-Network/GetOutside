2cd34f508bec1bb85c971fa127e3d3e06f33d650
GreenInfo Network <gin@greeninfo.org>
2014-07-03 12:36:58 -0700

    PlaceActivity model: changed the default sorting for nicer listings: name first, then time

--- application/models/placeactivity.php
+++ application/models/placeactivity.php
@@ -2,7 +2,7 @@
 class PlaceActivity extends DataMapper {
 
 var $table            = 'placeactivities';
-var $default_order_by = array('mon DESC','tue DESC','wed DESC','thu DESC','fri DESC','sat DESC','sun DESC','starttime');
+var $default_order_by = array('name', 'starttime', 'mon DESC','tue DESC','wed DESC','thu DESC','fri DESC','sat DESC','sun DESC');
 var $has_one          = array('place',);
 var $has_many         = array();
 
